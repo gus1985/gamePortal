541fc33bd0c31c2519f27fc6bcca56396e190e40
gustavo_lescano85@hotmail.com <gustavo_lescano85@hotmail.com>
2017-05-25 05:16:56 -0300

    Add JQuery animation for responsive nav-bar menu in custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,5 @@
+//ANIMATION FORM & SEARCH//
+
 $( document ).ready(function() {
 	$("#header-user").click(function(){
 		$("#header-form").slideToggle("slow");
@@ -9,3 +11,27 @@ $( document ).ready(function() {
 		$("#header-search").slideToggle("slow");
 	});
 	});
+
+//MENU RESPONSIVE//
+
+$( document ).ready(function() {
+	$("#nav-user").click(function(){
+		$("#nav-form").slideToggle("slow");
+	});
+	});
+
+$(function(){
+
+	$('#slide-submenu').on('click',function() {			        
+        $(this).closest('.list-group').fadeOut('slide',function(){
+        	$('.mini-submenu').fadeIn();	
+        });
+        
+      });
+
+	$('.mini-submenu').on('click',function(){		
+        $(this).next('.list-group').toggle('slide');
+        $('.mini-submenu').hide();
+	})
+})
+
