8069fae0cb5b9f13d516c7564fc17495a019100a
gustavo_lescano85@hotmail.com <gustavo_lescano85@hotmail.com>
2017-05-25 05:21:37 -0300

    custom.min.js updated

---
+++ js/custom.min.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+	$("#header-user").click(function(){
+		$("#header-form").slideToggle("slow");
+	});
+	});
+
+$( document ).ready(function() {
+	$("#search").click(function(){
+		$("#header-search").slideToggle("slow");
+	});
+	});
